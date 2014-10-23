84c57fa85e079919e1589938a74c5fba47347239
Simon Murtha Smith <simon@murtha-smith.com>
2014-10-22 22:55:09 -0400

    Two dev identities.

--- managers/Home Base/Home Base/SLIdentity.m
+++ managers/Home Base/Home Base/SLIdentity.m
@@ -15,7 +15,11 @@
     one.publicKey = @"3991d233de34b66ac1020304336e66148a1a9504c11cec42e562b355253a6afe";
     one.secretKey = @"e4923aba31c95f964934b07a0c6c9b2c03001b7cb9cc7b539c27307f42b193453991d233de34b66ac1020304336e66148a1a9504c11cec42e562b355253a6afe";
 
-    return [NSMutableArray arrayWithObjects:one, nil];
+    SLIdentity *two = [[SLIdentity alloc] init];
+    two.publicKey = @"351b3967461959ac25770abc66f21144a4231224bff33668ba5a8e4af9b610e4";
+    two.secretKey = @"40f7c9e3949600f562302adf333dfaecd656c6015f5b1e2ea3d57afd585086c9351b3967461959ac25770abc66f21144a4231224bff33668ba5a8e4af9b610e4";
+
+    return [NSMutableArray arrayWithObjects:one, two, nil];
 }
 
 + (SLIdentity*) identityForPublicKey:(NSString*)publicKey {
